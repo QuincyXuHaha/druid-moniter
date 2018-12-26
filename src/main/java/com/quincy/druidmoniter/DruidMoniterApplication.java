9e69c7890406392497c88a74eccb919ef1cacb21
quincy <quincy.xu@qq.com>
2018-12-26 10:21:18 +0800

    init

---
+++ src/main/java/com/quincy/druidmoniter/DruidMoniterApplication.java
@@ -0,0 +1,14 @@
+package com.quincy.druidmoniter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidMoniterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidMoniterApplication.class, args);
+    }
+
+}
+
